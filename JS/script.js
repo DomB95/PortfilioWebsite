ed15e960eb69d581c0e6b15f6415dc73d4fe63e1
DomB95 <drb0708@hotmail.com>
2025-07-24 19:58:33 -0400

    Resized for mobile

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
+/* ===========================================================================ACTIVE NAVBAR=====================================================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
 /* ===========================================================================scroll sections active links =====================================================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -21,5 +32,10 @@ window.onscroll = () => {
 /* =========================================================================== stick Navbar =====================================================*/
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+/* =========================================================================== remove nav and active  =====================================================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+
 
 }
