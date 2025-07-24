e6c247a7732fd6c69647ef35dd0dbcfb88f9e9b2
DomB95 <drb0708@hotmail.com>
2025-07-24 00:00:30 -0400

    Starting the javascript page

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.clientHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('.header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+
+
+
+
+}
