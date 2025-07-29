11c6c625f5d1332fa6bdb6a17df8fb3ea0ecc117
DomBie <drb0708@hotmail.com>
2025-07-29 02:01:30 -0400

    Template Done

--- JS/script.js
+++ JS/script.js
@@ -5,7 +5,7 @@ let navbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-}
+};
 
 
 
@@ -38,4 +38,25 @@ window.onscroll = () => {
 
 
 
-}
+};
+
+/* =========================================================================== scroll reveal =====================================================*/
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+}); 
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* =========================================================================== typed js =====================================================*/
+const typed = new Typed('.multiple-text', { strings : ['Software Developer', 'Web Developer', 'UI/UX Designer'],
+    typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true
+});
+
+
+/* =========================================================================== button handler =====================================================*/ 
